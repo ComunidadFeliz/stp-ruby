fb4da2f574a47b8f0ee59de5008b31440ba4b974
Mauricio Murga <murga.mauricio@gmail.com>
2019-02-01 14:59:47 -0600

    ignore ssl

--- lib/stp/client.rb
+++ lib/stp/client.rb
@@ -5,7 +5,12 @@ module Stp
     attr_accessor :client
 
     def initialize
-      @client = Savon.client(wsdl: Stp.configuration.wsdl, ssl_verify_mode: :none)
+      # @client = Savon.client(wsdl: Stp.configuration.wsdl, ssl_verify_mode: :none)
+      @client = Savon::Client.new do
+        wsdl.document = Stp.configuration.wsdl
+        http.auth.ssl.verify_mode = :none
+        http.auth.ssl.ssl_version = :SSLv3
+      end
     end
   end
 end
